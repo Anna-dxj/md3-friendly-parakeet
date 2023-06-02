f05f07baf413a75da0bf962149f55e756ad5d226
Xiaojia <annacl1822@gmail.com>
2023-06-01 18:39:23 -0700

    fix up code, add form

--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,104 @@
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector('#generate');
+const clearBtn = document.querySelector('#clear');
+const newGenBtn = document.querySelector('#new-pw');
+const noCharsModal = document.querySelector('#no-chars');
+const noCharsBtn = document.querySelector('#no-chars-btn');
+const noLengthModal = document.querySelector('#no-length')
+const noLengthBtn = document.querySelector('#no-length-btn');
 
-// Write password to the #password input
-generateBtn.addEventListener("click", function() {
-  generatePassword();
+const formDiv = document.querySelector('#form-container');
+const pwDiv = document.querySelector('#password-container')
+const pwContainer = document.querySelector('#password');
+
+const clearForm = () => {
+    let pwLength = document.querySelector('#pw-length');
+    const chosenUpper = document.querySelector('#uppercase');
+    const chosenLower = document.querySelector('#lowercase');
+    const chosenNumber = document.querySelector('#numbers');
+    const chosenSpecial = document.querySelector('#special');
+
+    pwLength.value = '';
+    chosenUpper.checked = false;
+    chosenLower.checked = false;
+    chosenNumber.checked = false;
+    chosenSpecial.checked = false;
+};
+
+const handleGeneratePassword = () => {
+    const pwLength = document.querySelector('#pw-length').value;
+    const chosenUpper = document.querySelector('#uppercase');
+    const chosenLower = document.querySelector('#lowercase');
+    const chosenNumber = document.querySelector('#numbers');
+    const chosenSpecial = document.querySelector('#special');
+
+    const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    const lower = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "1234567890";
+    const special = '!@#$%^&*(){}[]=<>/.,';
+    
+    let passwordCharacters = '';
+    let newPassword = ''; 
+
+    if (!pwLength || pwLength === null) {
+        noLengthModal.setAttribute('class', 'warning-modal')
+        return;
+    }
+    if (!chosenUpper.checked && !chosenLower.checked && !chosenNumber.checked && !chosenSpecial.checked) {
+        noCharsModal.setAttribute('class', 'warning-modal')
+        return;
+    }
+
+    if (chosenUpper.checked) {
+        passwordCharacters += upper;
+    } 
+    if (chosenLower.checked) {
+        passwordCharacters += lower;
+    }
+    if (chosenNumber.checked) {
+        passwordCharacters += numbers;
+    }
+    if (chosenSpecial.checked) {
+        passwordCharacters += special;
+    }
+
+    for (let i = 0; i < pwLength; i++) {
+        const random = Math.floor(Math.random() * passwordCharacters.length);
+        newPassword += passwordCharacters.substring(random, random+1);
+    };
+
+    formDiv.setAttribute('class', 'hidden');
+    pwContainer.textContent = newPassword;
+    pwDiv.setAttribute('class', 'card');
+}
+
+const handleClearForm = () => {
+    clearForm();
+    formDiv.setAttribute('class', 'card');
+    pwDiv.setAttribute('class', 'hidden');
+    
+};
+
+
+clearBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleClearForm();
 });
 
-//Valid characters: 
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "1234567890";
-var special = "!@#$%^&*(){}[]=<>/.,";
+generateBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleGeneratePassword();
+});
 
-function generatePassword () {
-    var passwordCharacters = ''
-    var newPassword = '';
-    //choose how long password 
-    var chosenLength = window.prompt("Choose a length of password (8~128 characters):")
-    //limit choices to valid number choices 
-    if (chosenLength<8 || chosenLength>128){
-      alert("Please choose a password length of 8 to 128");
-      return; 
-    };
-    //user choices
-    var chosenUpper = window.confirm("Would you like to use uppercase letters?");
-    var chosenLower = window.confirm("Would you like to use lowercase letters?");
-    var chosenNumber = window.confirm("Would you like to use numbers?");
-    var chosenSpecial = window.confirm("Would you like to use special characters?");
-    //limits valid characters to make password out of 
-    if (chosenUpper){  
-        passwordCharacters += upper
-    } if (chosenLower){
-        passwordCharacters += lower
-    } if (chosenNumber){
-        passwordCharacters += numbers
-    } if (chosenSpecial){
-        passwordCharacters += special
-    } else {
-        alert("Please choose valid data type")
-    }
-    //randomizes and generates password
-    for (var i = 0; i < chosenLength; i++){
-        var random = Math.floor(Math.random()*passwordCharacters.length);
-        newPassword += passwordCharacters.substring(random, random+1); 
-      };
-    //puts password on screen 
-    document.querySelector('#password').textContent = newPassword;
-};
+newGenBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    handleClearForm();
+})
+noLengthBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    noLengthModal.setAttribute('class', 'hidden');
+});
+
+noCharsBtn.addEventListener('click', (event) =>{
+    event.preventDefault();
+    noCharsModal.setAttribute('class', 'hidden');
+});
